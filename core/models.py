b446a0d39f3515b8a5fb738594d0fea31183ebd3
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-04 01:33:51 +0530

    model and uv init

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    HOUSE_CHOICE = [
+        ('amritamayi','AMRITAMAYI'),
+        ('anandamayi','ANANDAMAYI'),
+        ('chinmati','CHINMAYI'),
+        ('jyothirmati','JYOTHIRMAYI')
+    ]
+    roll_number = models.CharField(max_length=200,unique=True,null=False)
+    house = models.CharField(max_length=200,choices=HOUSE_CHOICE)
+
+    def __str__(self):
+        return self.roll_number
