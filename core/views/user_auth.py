0f1fbf96523b81443fe837158d20034aebbd9ad5
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-05 11:31:59 +0530

    user login done

---
+++ core/views/user_auth.py
@@ -0,0 +1,56 @@
+
+
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.contrib.auth import login, authenticate
+from ..models import User
+
+
+
+
+@csrf_exempt
+@require_POST
+def user_login(request):
+
+    try:
+        data = json.loads(request.body)
+        
+
+        if not data['email'] or not data['password']:
+            return JsonResponse({
+                "error" : "Email and password required"
+            },status=400)
+        
+        user_obj = User.objects.get(email=data['email'])
+        if not user_obj :
+            return JsonResponse(
+                {"error": "Invalid credentials"},
+                status=401
+            )
+        user = authenticate(
+            request,
+            username=user_obj.username,
+            password=data['password']
+        )
+
+        if user is None:
+            return JsonResponse(
+                {"error": "Invalid credentials"},
+                status=401
+            )
+        login(request,user)
+        return JsonResponse(
+            {
+                "message": "Login successful",
+                "user": {
+                    "roll_number": user.roll_number,
+                    "email": user.email,
+                    "house": user.house
+                }
+            },
+            status=200
+        )
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON"}, status=400)
