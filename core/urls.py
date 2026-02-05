ccff24db9409ba48c6971eccde2b27ad2616eae7
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-06 01:36:34 +0530

    csrf token added

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views.home_view import check_status_api
+from .views.home_view import check_status_api,get_csrf
 from .views.user_register import user_register
+from .views.user_auth import user_login
+
 
 urlpatterns = [
     path("healthcheck/", check_status_api, name="check_status_api"),
     path("register/", user_register, name="user_register"),
+    path("login/", user_login, name="user_login"),
+    path("csrf/", get_csrf, name="get_csrf"),
+
+
 ]
--- core/views/home_view.py
+++ core/views/home_view.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 @csrf_exempt
@@ -11,3 +12,7 @@ def check_status_api(request):
     },
     status=200
     )
+
+@ensure_csrf_cookie
+def get_csrf(request):
+    return JsonResponse({"message": "CSRF cookie set"})
