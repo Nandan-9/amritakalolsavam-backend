bf84fd7bb4e9aa826839a24bf318274da6dc7149
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-06 23:22:15 +0530

    migrated frome sessions to JWT auth

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from .views.home_view import check_status_api,get_csrf
 from .views.user_register import user_register
-from .views.user_auth import user_login
-from .views.user_details import user_details
+from .views.user_details import UserDetails
+from .views.email_otp_auth import send_otp
+
 
 
 
 urlpatterns = [
     path("healthcheck/", check_status_api, name="check_status_api"),
     path("register/", user_register, name="user_register"),
-    path("login/", user_login, name="user_login"),
-    path("details/",user_details, name="user_details"),
+    path("send-otp/", send_otp, name="send_otp"),
+    path("details/", UserDetails.as_view(), name="user_details"),
     path("csrf/", get_csrf, name="get_csrf"),
 
 
