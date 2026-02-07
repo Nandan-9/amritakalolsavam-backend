dd9f0ab0d771961d2437661d92a1f82b019d4bfd
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-08 03:30:17 +0530

    event reg url

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import get_all_events
+from  registrations.views.register_event import EventRegistrationView
 
 urlpatterns = [
     path("all/", get_all_events, name="get_all_events"),
+    path("<int:event_id>/registrations/",EventRegistrationView.as_view(),name="event-registrations"),
 
 ]
