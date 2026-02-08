061f4633804b980ca2983ef4b3c413bfb1de0c96
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-09 00:40:31 +0530

    id changed to roll_number

--- core/helper/get_user_by_id.py
+++ core/helper/get_user_by_id.py
@@ -2,8 +2,8 @@
 
 from ..models import User
 
-def get_user_by_id(id):
+def get_user_by_id(roll_number):
     try:
-        return User.objects.get(id=id)
+        return User.objects.get(roll_number=roll_number)
     except User.DoesNotExist:
         return None
