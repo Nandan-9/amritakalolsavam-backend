d3297612976970ca5700076d41fc8c929940ec56
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-12 02:25:52 +0530

    otp logic deleted

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
-from .views.home_view import check_status_api,get_csrf
+from .views.home_view import check_status_api
 from .views.user_register import user_register
 from .views.user_details import UserDetails
-from .views.email_otp_auth import send_otp
-
 
 
 
 urlpatterns = [
     path("healthcheck/", check_status_api, name="check_status_api"),
     path("students/register/", user_register, name="user_register"),
-    path("send-otp/", send_otp, name="send_otp"),
     path("details/", UserDetails.as_view(), name="user_details"),
-    path("csrf/", get_csrf, name="get_csrf"),
 
 
 ]
