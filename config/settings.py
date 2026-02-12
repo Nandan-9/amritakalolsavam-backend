cff2edd5749fd95a53de32d05d97842973694ce5
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-12 20:34:44 +0530

    last final final settings.py commit

--- config/settings.py
+++ config/settings.py
@@ -110,12 +110,6 @@ AUTH_USER_MODEL = 'core.User'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     "default": {
