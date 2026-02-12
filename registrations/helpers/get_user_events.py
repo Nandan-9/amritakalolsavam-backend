2540a3d430e06adb889721e9bca29091a2323bc5
a.d.devnandan <a.d.devnandan@gmail.com>
2026-02-12 19:35:54 +0530

    participant reg details added

--- registrations/helpers/get_user_events.py
+++ registrations/helpers/get_user_events.py
@@ -1,17 +1,29 @@
-
-
-from ..models import EventRegistration
+from ..models import EventRegistration, GroupParticipants
 
 
 def get_user_events(user):
 
-    registrations = EventRegistration.objects.filter(
+    # Registrations where user is leader
+    leader_regs = EventRegistration.objects.filter(
         registered_by=user
-    ).values(
-        "event__id",
-        "event__name",
-        "event__venue",
-        "chest_number",
     )
 
-    return list(registrations)
+    # Registrations where user is participant
+    participant_regs = EventRegistration.objects.filter(
+        participants__user=user
+    )
+
+    # Merge both (remove duplicates if user is both leader & participant)
+    all_regs = (leader_regs | participant_regs).distinct()
+
+    return list(
+        all_regs.values(
+            "event__id",
+            "event__name",
+            "event__venue",
+            "event__event_date",
+            "event__participation_mode",
+            "chest_number",
+            "registered_by__username",
+        )
+    )
